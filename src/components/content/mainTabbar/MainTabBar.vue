ba3372e9b579b11567b0df9e7c4677b0f9ddd859
Lyn <2420916021@qq.com>
2021-08-13 09:58:05 +0800

    tabbar完成

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path='/home'>
+        <template v-slot:item-icon
+          ><img src="~assets/images/tabbar/home.svg" alt=""
+        /></template>
+        <template v-slot:item-icon-active
+          ><img src="~assets/images/tabbar/home_active.svg" alt=""
+        /></template>
+        <template v-slot:item-text><div>首页</div></template>
+      </tab-bar-item>
+
+      <tab-bar-item path='/category'>
+        <template v-slot:item-icon
+          ><img src="~assets/images/tabbar/category.svg" alt=""
+        /></template>
+        <template v-slot:item-icon-active
+          ><img src="~assets/images/tabbar/category_active.svg" alt=""
+        /></template>
+        <template v-slot:item-text><div>分类</div></template>
+      </tab-bar-item>
+
+      <tab-bar-item path='/cart'>
+        <template v-slot:item-icon
+          ><img src="~assets/images/tabbar/cart.svg" alt=""
+        /></template>
+        <template v-slot:item-icon-active
+          ><img src="~assets/images/tabbar/cart_active.svg" alt=""
+        /></template>
+        <template v-slot:item-text><div>购物车</div></template>
+      </tab-bar-item>
+
+      <tab-bar-item path='/profile'>
+        <template v-slot:item-icon
+          ><img src="~assets/images/tabbar/profile.svg" alt=""
+        /></template>
+        <template v-slot:item-icon-active
+          ><img src="~assets/images/tabbar/profile_active.svg" alt=""
+        /></template>
+        <template v-slot:item-text> <div>我的</div></template>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+<script>
+import TabBar from "components/common/tabbar/TabBar.vue";
+import TabBarItem from "components/common/tabbar/TabBarItem.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  components: {
+    TabBar,
+    TabBarItem
+  },
+  methods: {}
+};
+</script>
+<style></style>
